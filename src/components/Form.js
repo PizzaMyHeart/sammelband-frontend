dc05326e41eeee550dbd5e6a78800ffb66806d7b
PizzaMyHeart <ian_tan@hotmail.com>
2022-02-09 23:13:33 +0000

    fixed delete button action - moved outside of form element

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,13 @@
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 function Form() {
     const [urls, setUrls] = useState('');
 
-    const handleSubmit = e => {
+    
+
+    
+    const onSubmit = e => {
         let payload = {
             urls: urls,
             test: 'test'
@@ -20,7 +24,25 @@ function Form() {
             body: JSON.stringify(payload)
         })
     }
-
+    
+    /* 
+    // react-hook-form version
+    const onSubmit = data => {
+        console.log(data['urls']);
+        fetch('/submit', {
+            credentials: 'same-origin',
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                urls: data['urls'],
+                colorTheme: data['color-theme']
+            })
+        })
+    }
+    */
     const deleteFile = () => {
         fetch('/delete', {
             credentials: 'same-origin',
@@ -30,32 +52,38 @@ function Form() {
     }
 
     return ( 
-        <form onSubmit={ handleSubmit }>
-            <label>
-                URLs:
-                <textarea 
-                    id="url-input" 
-                    onChange={ e => {setUrls(e.target.value)} } 
-                    required
-                    pattern="^http.*"
-                    />
-            </label>
-            <div>
-                <input type="radio" value="dark" name="color-theme"/> Dark mode
-                <input type="radio" value="light" name="color-theme"/> Light mode
-            </div>
-            <div> 
-                <input type="radio" value="html" name="format"/> HTML
-                <input type="radio" value="pdf" name="format"/> PDF 
-                <input type="radio" value="epub" name="format"/> EPUB
-                <input type="radio" value="markdown" name="format"/> Markdown 
-                <input type="radio" value="txt" name="format"/> TXT 
-            </div>
-                
+        <>
+            <form onSubmit={ onSubmit }>
+                <label>
+                    URLs:
+                    <textarea 
+                        id="url-input" 
+                        onChange={ e => {setUrls(e.target.value)} } 
+                        /*{...register('urls')}*/
+                        />
+                </label>
+                <div>
+                    <input type="radio" value="dark" name="color-theme"/> Dark mode
+                    <input type="radio" value="light" name="color-theme"/> Light mode
+                </div>
+                <div>
+                    <input type="radio" value="serif" name="font"/> Serif
+                    <input type="radio" value="sans-serif" name="font"/> Sans-serif
+                </div>
+                <div> 
+                    <input type="radio" value="html" name="format"/> HTML
+                    <input type="radio" value="pdf" name="format"/> PDF 
+                    <input type="radio" value="epub" name="format"/> EPUB
+                    <input type="radio" value="markdown" name="format"/> Markdown 
+                    <input type="radio" value="txt" name="format"/> TXT 
+                </div>
+                    
 
-            <input type="submit" value="Submit" />
+                <input type="submit" value="Submit" />
+                
+            </form>
             <button onClick={ deleteFile }>Delete</button>
-        </form>
+        </>
     )
 }
 
